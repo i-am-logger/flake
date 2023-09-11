95a07d26370e88830bda0d017f8a81aeb19efa32
SnIcK <ido.samuelson@gmail.com>
2023-09-11 12:32:22 -0500

    added: dialout group to user

--- hosts/users/snick.nix
+++ hosts/users/snick.nix
@@ -1,17 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.snick = {
     name = "snick";
     initialPassword = "";
     isNormalUser = true;
     description = "Ido Samuelson";
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "udev" ];
+    extraGroups = ["networkmanager" "wheel" "video" "audio" "udev" "dialout"];
     shell = pkgs.fish;
   };
 }
