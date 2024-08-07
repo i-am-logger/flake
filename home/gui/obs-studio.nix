e27004c061f0db984bfc5a7bd35f252136ffd64a
Logger <ido.samuelson@gmail.com>
2024-08-07 15:58:49 -0600

    update

--- home/gui/obs-studio.nix
+++ home/gui/obs-studio.nix
@@ -1,7 +1,11 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
+
+      # for terminal fun
+      pkgs.twitch-tui # twitch chat in terminal
+      pkgs.streamlink # streamlink for live videostream
       wlrobs
       obs-teleport
     ];
