81773ccc18644cad0eddc621680fd709f9fb254d
Logger <ido.samuelson@gmail.com>
2024-08-22 11:59:34 -0600

    hyprland

--- home/gui/hyprland/config/decorations.nix
+++ home/gui/hyprland/config/decorations.nix
@@ -1,16 +1,16 @@
 {
   decoration = {
-    active_opacity = 0.85;
-    inactive_opacity = 0.85;
-    fullscreen_opacity = 0.85;
-    rounding = 8;
+    active_opacity = 0.95;
+    inactive_opacity = 0.95;
+    fullscreen_opacity = 0.95;
+    # rounding = 8;
     dim_inactive = true;
     dim_strength = 0.3; # 0.0 ~ 1.0
     blur = {
-      # enabled = false;
+      enabled = true;
       # brightness = 1.0
       # contrast = 2.0
-      size = 2;
+      size = 3;
     };
     # col.shadow = rgba(00000099)
   };
