be39d124becdbcc223ebeaebb0f97ac6296e3a2f
Logger <1440852+i-am-logger@users.noreply.github.com>
2025-03-08 21:53:08 -0700

    Update nixos_enter.sh

--- scripts/nixos_enter.sh
+++ scripts/nixos_enter.sh
@@ -3,3 +3,6 @@ mount /dev/nvme0n1p4 -o subvol=DATA/home /mnt/home
 mount /dev/nvme0n1p4 -o subvol=SYSTEM/nix /mnt/nix
 #vmount /dev/nvme0n1p2 -o subvol=swap /mnt/swap
 mount /dev/nvme0n1p1 /mnt/boot
+
+
+export NIX_CONFIG="experimental-features = nix-command flakes"
