7a9ac4441c9ea8270d645afc06340b5d8d7a8b90
logger <i-am-logger@users.noreply.github.com>
2025-04-08 21:41:51 -0600

    fix: yubikey

--- Systems/yoga/yubikey.nix
+++ Systems/yoga/yubikey.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  # Core YubiKey services
   services.pcscd = {
     enable = true;
     plugins = [ pkgs.ccid ];
@@ -14,17 +13,11 @@
   services.yubikey-agent.enable = false; # Explicitly disable in favor of gpg-agent
   hardware.gpgSmartcards.enable = true;
 
-  # services.udev.extraRules = ''
-  #   # YubiKey rules to allow both pcscd and direct access
-  #   ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0407", TAG+="systemd", ENV{SYSTEMD_WANTS}="pcscd.service", GROUP="users", MODE="0660"
-  # '';
-  # YubiKey-related packages and support
   services.udev.packages = [
     pkgs.yubikey-personalization
     pkgs.libu2f-host
   ];
 
-  # Persistence configuration (this is good as is)
   environment.persistence."/persist" = {
     directories = [ "/persist/yubikey" ];
     users.logger = {
@@ -36,14 +29,6 @@
     };
   };
 
-  # Essential packages only
-  # environment.systemPackages = with pkgs; [
-  #   # yubikey-manager
-  #   # yubikey-personalization
-  #   # pcsc-tools
-  # ];
-
-  # PAM authentication configuration (this is good as is)
   security.pam = {
     services = {
       login.u2fAuth = true;
